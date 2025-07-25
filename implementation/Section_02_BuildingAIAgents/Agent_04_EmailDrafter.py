eac9dae5a0e41dfb70ae683119e55585208351a2
Dhanesh Gujrathi <57758116+dhanesh24g@users.noreply.github.com>
2025-07-25 08:50:22 +0800

    Agent-04 | Added tools

--- implementation/Section_02_BuildingAIAgents/Agent_04_EmailDrafter.py
+++ implementation/Section_02_BuildingAIAgents/Agent_04_EmailDrafter.py
@@ -6,3 +6,51 @@ from langgraph.graph import StateGraph, END
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode
 from dotenv import load_dotenv
+
+load_dotenv()
+
+# Global variable to store the document content (Alternate to InjectedState)
+document_content = ""
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+#------------ Tools Section Start ------------#
+
+@tool
+def update(content: str) -> str:
+    """Updates the document with provided content."""
+    global document_content
+    document_content = content
+
+    return f"The document has been updated with the following content:\n{document_content}"
+
+@tool
+def save(filename: str) -> str:
+    """Saves the document with a suitable Text filename and finishes processing.
+
+    Args:
+        filename: Name for the Text file.
+    """
+
+    global document_content
+
+    if not filename.endswith(".txt"):
+        filename += ".txt"
+
+    try:
+        with open(filename, 'w') as file:
+            file.write(document_content)
+            print(f"\n💾Document has been saved successfully to {filename}.")
+
+            return f"Document has been saved successfully to {filename}."
+
+    except Exception as e:
+        return f"An error occurred while saving the document: {str(e)}"
+
+#------------ Tools Section End ------------#
+
+toolkit = [update, save]
+
+llm = ChatOpenAI(model="gpt-4o").bind_tools(tools=toolkit)
+
